e41e90004b57d9163a148da34aec42437447216d
Rob Taft <rtaft@navisite.com>
2020-10-03 07:11:19 -0400

    fixes

--- traders/test-1/subfolder/helper.py
+++ traders/test-1/subfolder/helper.py
@@ -1,4 +1,5 @@
 from traders.interface import TraderInterface, TraderSchema
+from marshmallow import Schema, fields, validate, EXCLUDE
 
 class MyTraderSchema2(TraderSchema):
     symbol = fields.String(required=True, metadata=dict(notes="Symbol of the stock to buy."))
